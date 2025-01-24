500cfd56b81f8b0807a20f46d8a74a3426a477d2
AceReX <ace.rexnoir@gmail.com>
2025-01-24 22:11:58 +0530

    feat(shared): add package.json, tsconfig, and options for audio/video formats and qualities

---
+++ packages/shared/src/options.ts
@@ -0,0 +1,33 @@
+export const outputFormats = [
+  "mp3",
+  "mp4",
+  "webm",
+  "ogg",
+  "wav",
+  "m4a",
+  "flac",
+  "opus",
+  "aac",
+  "wma",
+];
+
+export const videoQualities = [
+  "Best Quality",
+  "144p",
+  "240p",
+  "360p",
+  "480p",
+  "720p",
+  "1080p",
+  "1440p",
+  "2160p",
+  "4320p",
+];
+
+export const audioQualitites = [
+  "Best Quality",
+  "128kbps",
+  "192kbps",
+  "256kbps",
+  "320kbps",
+];
