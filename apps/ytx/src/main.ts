90b2742e738b5193fef0152bc8a4914de5033aa5
AceReX <ace.rexnoir@gmail.com>
2025-01-24 10:53:41 +0530

    feat(ytx): integrate PrimeVue and TailwindCSS with custom theme configuration

--- apps/ytx/src/main.ts
+++ apps/ytx/src/main.ts
@@ -1,14 +1,26 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
+import ytxTheme from './ytx.theme.'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue, {
+  theme: {
+    preset: ytxTheme,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'base, primevue, components, utilities',
+      },
+    },
+  },
+})
 app.mount('#app')
