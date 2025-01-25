34363f1e40054cf3251d62fd4df6b6d7201cbf6a
AceReX <ace.rexnoir@gmail.com>
2025-01-25 16:57:49 +0530

    feat(shared): enhance GenerateRequestSchema with additional fields and default values

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -5,5 +5,13 @@ export const GenerateRequestSchema = z.object({
   format: z.enum(outputFormats, { message: "Invalid format" }),
   videoQuality: z.enum(videoQualities, { message: "Invalid video quality" }),
   audioQuality: z.enum(audioQualitites, { message: "Invalid audio quality" }),
-  saveThumbnail: z.boolean(),
+  saveThumbnail: z.boolean().default(false),
+  startTime: z.number().nullable().optional(),
+  endTime: z.number().nullable().optional(),
+  youtubeUrl: z
+    .string()
+    .regex(
+      /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?v=|embed\/)|youtu\.be\/|music\.youtube\.com\/watch\?v=)([\w-]{11})/,
+      { message: "Invalid YouTube URL" }
+    ),
 });
