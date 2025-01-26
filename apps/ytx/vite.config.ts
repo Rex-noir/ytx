797c0585aaa6d907307ae5d59e48d0db6ce66710
AceReX <ace.rexnoir@gmail.com>
2025-01-26 17:57:19 +0530

    feat(app): update Vite configuration to include PrimeVue auto-import resolver and enhance component handling

--- apps/ytx/vite.config.ts
+++ apps/ytx/vite.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import tailwindcss from '@tailwindcss/vite'
-import "tailwindcss-primeui"
 import vue from '@vitejs/plugin-vue'
+import 'tailwindcss-primeui'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+    vueDevTools(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
